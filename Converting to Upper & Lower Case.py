91947548aeb035cf219b9e278e307f7c42434b35
heetsolankiii <heetsolanki2006@gmail.com>
2024-01-07 19:44:54 +0530

    Create Converting to Upper & Lower Case.py

---
+++ Converting to Upper & Lower Case.py
@@ -0,0 +1,4 @@
+userInput = input("Enter a sentence: ") # Asking user for input
+print(userInput.upper())    # Converting to UPPER CASE & printing
+userInput = input("Enter a sentence: ") # Asking user for input
+print(userInput.lower())    # Converting to lower case & printing
