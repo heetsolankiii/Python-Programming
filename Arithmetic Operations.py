19fa40f83ee7185d8a340a96bf1787aad9e78859
heetsolankiii <heetsolanki2006@gmail.com>
2024-01-07 13:24:15 +0530

    Added Modulus Operation & Comments

--- Arithmetic Operations.py
+++ Arithmetic Operations.py
@@ -1,12 +1,12 @@
 a=10
 b=3
-print(a, "+", b, "=", a+b) # Addition
-print(a, "-", b, "=", a-b) # Subtraction
-print(a, "*", b, "=", a*b) # Multiplication
-print(a, "/", b, "=", a/b) # Division
-print(a, "//", b, "=", a//b) # Floor Division
-print(a, "**", b, "=", a**b) # Exponential
-print(a, "%", b, "=", a%b) # Modulus
+print(a, "+", b, "=", a+b)      # Addition
+print(a, "-", b, "=", a-b)      # Subtraction
+print(a, "*", b, "=", a*b)      # Multiplication
+print(a, "/", b, "=", a/b)      # Division
+print(a, "//", b, "=", a//b)    # Floor Division
+print(a, "**", b, "=", a**b)    # Exponential
+print(a, "%", b, "=", a%b)      # Modulus
 
 
 
