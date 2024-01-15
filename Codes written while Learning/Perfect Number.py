1192a6e6455068d93afaae6d32003338f46336ac
heetsolankiii <heetsolanki2006@gmail.com>
2024-01-15 18:59:28 +0530

    Added Comments

--- Codes written while Learning/Perfect Number.py
+++ Codes written while Learning/Perfect Number.py
@@ -1,11 +1,11 @@
-number = int(input("Enter a number: "))
+number = int(input("Enter a number: "))     # Asking user input
 sum = 0
-for i in range(1, number):
-  if number%i==0:
-    sum = sum + i
+for i in range(1, number):      # Starting the loop
+  if number%i==0:       # Checking for condition
+    sum = sum + i       # Adding i to sum
   else:
     continue
-if sum==number:
-  print("It is a perfect number.")
+if sum==number:     # Checking for condition
+  print("It is a perfect number.")      # True Statement
 else:
-  print("It is not a perfect number.")
+  print("It is not a perfect number.")      # False Statement
